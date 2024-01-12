70eb13dd7f554c0fa51b31ca3a3416b2dcc369b5
gayoung0619 <did-619@naver.com>
2024-01-12 14:26:08 +0900

    feat: TMDB popular아이템 fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import HomePage from "./pages/HomePage";
 import BookPage from "./pages/BookPage";
 import ConfirmPage from "./pages/ConfirmPage";
 import Header from "./components/Header/Header";
 
 function App() {
+  const queryClient = new QueryClient();
 
   const router = createBrowserRouter([
     {
@@ -18,10 +20,10 @@ function App() {
   ]);
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Header />
       <RouterProvider router={router} />
-    </>
+    </QueryClientProvider>
   )
 }
 
--- src/components/Book/SeatSelectior.tsx
+++ src/components/Book/SeatSelectior.tsx
@@ -1,9 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
+import { movieFetcher } from "../../utils/http";
+
 const SeatSelectior = () => {
+  const { data } = useQuery({
+    queryKey: ['movie'],
+    queryFn: movieFetcher
+  });
+
+  console.log(data)
 
   return (
-    <h1 className="text-2xl font-bold italic">
-      자리선택 컴포넌트
-    </h1>
+    <>
+      <h1 className="text-2xl font-bold italic">
+        자리선택 컴포넌트
+      </h1>
+      {data.results.map((item) => <li>{item}</li>)}
+    </>
+
   )
 }
 export default SeatSelectior;
