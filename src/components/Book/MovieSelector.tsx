c24845a72da39a2d7933590c69acec01c0bf38ac
gayoung0619 <did-619@naver.com>
2024-01-16 15:18:03 +0900

    feat: SelectArea 컴포넌트로 추가

--- src/components/Book/MovieSelector.tsx
+++ src/components/Book/MovieSelector.tsx
@@ -1,10 +1,9 @@
 import { useState } from 'react';
 import Box from "../UI/Box";
 import AgeChip from '../Common/AgeChip';
-import { FaArrowRight } from "react-icons/fa";
-import { FaAngleRight } from "react-icons/fa";
 import { groupedTheater } from '../../data/groupedTheater';
 import { getDates } from '../../utils/getDates';
+import SelectArea from './SelectArea'
 
 const MovieSelector = () => {
   const dates = getDates();
@@ -130,21 +129,7 @@ const MovieSelector = () => {
           </div>
         </div>
       </div >
-      <div className='flex justify-between items-center w-full h-[100px] mt-2 bg-gray-200 text-black-1'>
-        <div className='flex flex-row items-center gap-5 ml-5'>
-          <span>영화선택</span>
-          <FaAngleRight className='' />
-          <span>극장선택</span>
-          <FaAngleRight />
-          <span>좌석선택</span>
-          <FaAngleRight />
-          <span>예매</span>
-        </div>
-        <div className='flex flex-col justify-center items-center w-[85px] h-[85px] mr-3 border border-black-1 rounded-xl cursor-pointer'>
-          <FaArrowRight className='text-3xl' />
-          <span className='pt-2 font-bold'>좌석선택</span>
-        </div>
-      </div>
+      <SelectArea />
     </Box >
   )
 }
