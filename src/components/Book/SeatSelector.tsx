5b57d909cf01e6de0291701327ea409ae055a7bb
gayoung0619 <did-619@naver.com>
2024-01-16 15:10:50 +0900

    feat: 영화포스터, 영화선택, 좌석선택 영역 추가

--- src/components/Book/SeatSelector.tsx
+++ src/components/Book/SeatSelector.tsx
@@ -1,13 +1,94 @@
 import Box from "../UI/Box";
+import '../../style/SeatSelector.css'
 
 const SeatSelector = () => {
   return (
     <>
       <Box>
-        <h1 className="text-2xl font-bold italic">
-          좌석선택 컴포넌트
-        </h1>
-      </Box>
+        <div className="flex justify-start">
+          <div className="flex-1 bg-gray-300 ">
+            <div className="moviePic">사진영역</div>
+          </div>
+
+          <div className="flex-1">
+            <div className="w-64">
+              <label htmlFor="select-movie" className="block text-sm font-medium text-gray-700">영화선택</label>
+              <select id="select-movie" name="영화"
+                      className="mt-1 block w-full p-2 border border-gray-300 rounded-md bg-white focus:outline-none focus:border-blue-500">
+                <option value="option1">옵션 1</option>
+                <option value="option2">옵션 2</option>
+                <option value="option3">옵션 3</option>
+              </select>
+            </div>
+
+            <div>
+              <div className="container">
+                <div className="screen">screen</div>
+              </div>
+              <div className="row">
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat"></div>
+              </div>
+              <div className="row">
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat occupied"></div>
+                <div className="seat occupied"></div>
+                <div className="seat"></div>
+                <div className="seat"></div>
+              </div>
+              <div className="row">
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat occupied"></div>
+                <div className="seat occupied"></div>
+              </div>
+              <div className="row">
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat"></div>
+              </div>
+              <div className="row">
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat occupied"></div>
+                <div className="seat"></div>
+                <div className="seat occupied"></div>
+                <div className="seat"></div>
+              </div>
+              <div className="row">
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat"></div>
+                <div className="seat occupied"></div>
+                <div className="seat occupied"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Box>가
     </>
   )
 }
