65779a73fd725821ff820fc1480393a710a93575
jye104 <84173008+jye104@users.noreply.github.com>
2024-01-25 16:48:22 +0900

    .env파일 생성 및 api 호출 작업

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,33 +1,61 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
+import { useQuery } from "@tanstack/react-query";
+import { movieFetcher } from "../../utils/http";
+
+const api_key = import.meta.env.VITE_API_KEY;
+
+// 아~~ 그렇긴 할 거 같아 ㅠ 지금 진도랑 프로젝트랑 안 맞아서
+// 앗 ㅋㅋ 알아두면 좋긴한데 사실 지금 당장은 몰라도 될거 같아 나도 잘 안쓰기도 하고
+// 어던 사람은 차라리 useMemo 안쓰는게 더 좋다고도 하구 강의라서 일단은 다 알려주나보다
+// 아진짜?ㅋㅋㅋㅋㅋ 그런가바 ㅜㅜ  그래서일단은듣는중 암튼. 이제 api호출하기 해볼게욤 근데 진짜 다써놓으셨다 ㅋㅋㅋㅋㅋ웅 할라고보니까 다해놓으셨네
+// 응응!! 정인씨?? 경쟁심 생기네 나도
+
 const Home = () => {
+  const { data } = useQuery({
+    queryKey: ["movie"],
+    queryFn: movieFetcher,
+  });
+
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    if (!data?.results) return;
+    setMovie(data?.results.slice(0, 4).map((movie) => ({ ...movie })));
+  }, [data?.results]);
+  // 리액트에서는 깊은 복사를 한 뒤 넣어주기
+
+  console.log(movie);
 
   return (
-    <h1 className="text-2xl font-bold italic">
-      홈컴포넌트
-    </h1>
-  )
-}
+    <>
+      <h2 className="mt-10 text-2xl font-bold">HOME</h2>
+      <div className="flex">
+        {movie.map((it) => (
+          <div className="movie group relative" key={it.id}>
+            <img
+              src={`https://image.tmdb.org/t/p/w300/${it.poster_path}`}
+              alt=""
+            />
+            <div className="detail-btns absolute z-10 top-0 left-0 w-full h-full hidden group-hover:flex flex-col justify-center items-center before:block before:w-full before:h-full before:absolute">
+              <a href="" className="relative z-10">
+                예매하기
+              </a>
+              <a href="" className="relative z-10">
+                상세보기
+              </a>
+            </div>
+            <div className="detail absolute top-full left-0 w-full">
+              <strong className="movie-title">{it.original_title}</strong>
+              <span className="release-date">{it.release_date}</span>
+              <span className="movie-ranked"></span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
 export default Home;
-
-
-// import { useQuery } from '@tanstack/react-query';
-// import { movieFetcher } from "../../utils/http";
-
-// const SeatSelectior = () => {
-//   const { data } = useQuery({
-//     queryKey: ['movie'],
-//     queryFn: movieFetcher
-//   });
-
-//   console.log(data)
-
-//   return (
-//     <>
-//       <h1 className="text-2xl font-bold italic">
-//         자리선택 컴포넌트
-//       </h1>
-//       {data.results.map((item) => <li>{item}</li>)}
-//     </>
-
-//   )
-// }
-// export default SeatSelectior;
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -12,7 +12,10 @@ const options = {
 
 export const getNowPlayingMovies = async () => {
   try {
-    const res = await fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}&language=${BASE_LANG}-${BASE_REGION}&page=1`, options);
+    const res = await fetch(
+      `${BASE_PATH}/movie/now_playing?api_key=${API_KEY}&language=${BASE_LANG}-${BASE_REGION}&page=1`,
+      options
+    );
     return await res.json();
   } catch (err) {
     console.error("Error fetching movie data:", err);
@@ -21,7 +24,10 @@ export const getNowPlayingMovies = async () => {
 
 export const getPopularMovies = async () => {
   try {
-    const res = await fetch(`${BASE_PATH}/movie/popular?api_key=${API_KEY}&language=${BASE_LANG}-${BASE_REGION}&page=1`, options);
+    const res = await fetch(
+      `${BASE_PATH}/movie/popular?api_key=${API_KEY}&language=${BASE_LANG}-${BASE_REGION}&page=1`,
+      options
+    );
     return res.json();
   } catch (err) {
     console.error("Error fetching movie data:", err);
@@ -30,37 +36,12 @@ export const getPopularMovies = async () => {
 
 export const getUpcomingMovies = async () => {
   try {
-    const res = await fetch(`${BASE_PATH}/movie/upcoming?api_key=${API_KEY}&language=${BASE_LANG}-${BASE_REGION}&page=1`, options);
+    const res = await fetch(
+      `${BASE_PATH}/movie/upcoming?api_key=${API_KEY}&language=${BASE_LANG}-${BASE_REGION}&page=1`,
+      options
+    );
     return res.json();
   } catch (err) {
     console.error("Error fetching movie data:", err);
   }
 };
-
-export const movieFetcher = async () => {
-  const apiUrl = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
-  const apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNjdhZmMwMzY4ZWE5ZmQ2MmY2NmJjM2Y2Yjc2YTNjYSIsInN1YiI6IjY1YTBiODE2MmNlZmMyMDEyNTM3NTM1NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tmguS-_f-tuirKlSzIh8zaQFtbBbzYcBf3RZUZObO8k';
-  // 예은
-
-  try {
-    const response = await fetch(apiUrl, {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${apiKey}`
-      },
-    });
-
-    if (!response.ok) {
-      // API 호출이 실패할 경우에 대한 처리
-      throw new Error(`Failed to fetch data. Status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    // 에러 발생 시 적절한 처리
-    console.error('Error fetching movie data:', error);
-    throw error;
-  }
-}
