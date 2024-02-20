605c874c6001c5486fe5b9514001c2b2e62ca880
gayoung0619 <did-619@naver.com>
2024-02-20 22:46:32 +0900

    feat: login 페이지 완료

--- src/pages/JoinPage.tsx
+++ src/pages/JoinPage.tsx
@@ -1,7 +1,9 @@
+import { useForm } from "react-hook-form";
+
 const JoinPage = () => {
   return (
     <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-      조인페이지
+      조인
     </div>
   );
 };
