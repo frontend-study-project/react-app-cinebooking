5b21001117ef76db801b91cc3c94a4936792a9c1
yeeun <84173008+jye104@users.noreply.github.com>
2024-03-12 18:35:03 +0900

    api 주소 뒤에 region 추가

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,11 +1,11 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
-const BASE_PATH = "https://api.themoviedb.org/3";
-const BASE_LANG = "ko";
-const BASE_REGION = "KR";
-const options = {
-  method: "GET",
+const BASE_PATH = 'https://api.themoviedb.org/3';
+const BASE_LANG = 'ko';
+const BASE_REGION = 'KR';
+export const options = {
+  method: 'GET',
   headers: {
-    accept: "application/json",
+    accept: 'application/json',
     Authorization: `Bearer ${API_KEY}`,
   },
 };
@@ -13,47 +13,47 @@ const options = {
 export const getNowPlayingMovies = async () => {
   try {
     const res = await fetch(
-      `${BASE_PATH}/movie/now_playing?api_key=${API_KEY}&language=${BASE_LANG}-${BASE_REGION}&page=1`,
+      `${BASE_PATH}/movie/now_playing?api_key=${API_KEY}&language=${BASE_LANG}-${BASE_REGION}&page=1&region=KR`,
       options
     );
     return await res.json();
   } catch (err) {
-    console.error("Error fetching movie data:", err);
+    console.error('Error fetching movie data:', err);
   }
 };
 
 export const getPopularMovies = async () => {
   try {
     const res = await fetch(
-      `${BASE_PATH}/movie/popular?api_key=${API_KEY}&language=${BASE_LANG}-${BASE_REGION}&page=1`,
+      `${BASE_PATH}/movie/popular?api_key=${API_KEY}&language=${BASE_LANG}-${BASE_REGION}&page=1&region=KR`,
       options
     );
     return res.json();
   } catch (err) {
-    console.error("Error fetching movie data:", err);
+    console.error('Error fetching movie data:', err);
   }
 };
 
 export const getUpcomingMovies = async () => {
   try {
     const res = await fetch(
-      `${BASE_PATH}/movie/upcoming?api_key=${API_KEY}&language=${BASE_LANG}-${BASE_REGION}&page=1`,
+      `${BASE_PATH}/movie/upcoming?api_key=${API_KEY}&language=${BASE_LANG}-${BASE_REGION}&page=1&region=KR`,
       options
     );
     return res.json();
   } catch (err) {
-    console.error("Error fetching movie data:", err);
+    console.error('Error fetching movie data:', err);
   }
 };
 
 export const getMovieDetails = async (id: number) => {
   try {
     const res = await fetch(
-      `${BASE_PATH}/movie/${id}?language=${BASE_LANG}-${BASE_REGION}&page=1`,
+      `${BASE_PATH}/movie/${id}?language=${BASE_LANG}-${BASE_REGION}&page=1&region=KR`,
       options
     );
     return res.json();
   } catch (err) {
-    console.error("Error fetching movie data:", err);
+    console.error('Error fetching movie data:', err);
   }
 };
