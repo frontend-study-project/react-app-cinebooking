bbe4536317af0667402bd1a3f3e4fbcaf5f9d874
yeeun <84173008+jye104@users.noreply.github.com>
2024-03-12 18:30:46 +0900

    Home 레이아웃 수정

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,8 @@
-import Home from "../components/Home/Home";
+import Home from '../components/Home/Home';
 
 const HomePage = () => {
   return (
-    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+    <div>
       <Home />
     </div>
   );
